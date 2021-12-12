23bffa93a700027c692ead63fe7c3895a85046d4
cisel-zumbul <68049174+cisel-zumbul@users.noreply.github.com>
2021-12-12 21:36:01 +0300

    Key_generation ama daha genel (128-192-256 bit)

--- Main.java
+++ Main.java
@@ -6,18 +6,34 @@ import java.util.Arrays;
 import java.util.Scanner;
 public class Main {
 
+    public static ArrayList<int[]> key_words = new ArrayList<int[]>();
+
     public static void main(String[] args) throws FileNotFoundException {
-	// write your code here
-        int a = Integer.parseInt("AF", 16);
-        System.out.println(a);
+        // write your code here
         File key_file = new File("/Users/cisel/Desktop/CMPE494/src/com/company/key_file.txt");
+
         Scanner scan = new Scanner(key_file);
         String key = scan.nextLine();
-        generate_key(1, key,32);
 
+        int[] w0 = new int[4];
+        int[] w1 = new int[4];
+        int[] w2 = new int[4];
+        int[] w3 = new int[4];
+
+        key_words.add(w0);
+        key_words.add(w1);
+        key_words.add(w2);
+        key_words.add(w3);
+
+        for (int i = 0; i < key.length(); i = i + 2) {  //dividing key to the words, words to the bytes
+            String hex = key.substring(i, i + 2);
+            int hx = Integer.parseInt(hex, 16);
+            key_words.get(i / 8)[(i / 2) % 4] = hx;
+        }
+        generate_key(4, 1);
     }
 
-    public static int generate_key(int round, String key, int len) throws FileNotFoundException {
+    /*public static int generate_key(int round, String key, int len) throws FileNotFoundException {
         int[] r_con= {1,2,4,8,16,32,64, 128, 27, 54, 108};
         if(key.length() != len){
             System.out.println("Invalid Key Length");
@@ -76,11 +92,58 @@ public class Main {
             }
         }
         return len;
-    }
+    }*/
 
     public static int[] substitude (int[] x) {
         int[] a = {183, 90, 157, 133};
         return a;
     }
 
+    public static void generate_key(int words, int round) { //words = 4 for 128-bit key, 6 for 192 bit key, 8 for 256 bit key
+
+        int[] old_key_part;
+        int[] old_key_part_to_modify;
+        int[] r_con= {1,2,4,8,16,32,64, 128, 27, 54, 108};
+
+        for(int i = round*4; i< (round+1)*4; i++ ){
+            int[] new_key_part = new int[4];
+            if(i < words){
+               new_key_part = key_words.get(i);
+            }
+            else if(i >= words && i%words == 0){
+                int[] temp_arr = new int[4];
+                old_key_part_to_modify = key_words.get(i-1);
+                for(int k=0; k<4; k++){
+                    temp_arr[(k+3)%4] = old_key_part_to_modify[k];
+                }
+                temp_arr = substitude(temp_arr);
+                temp_arr[0] = temp_arr[0] ^ r_con[round-1];
+                old_key_part = key_words.get(i-words);
+                for(int k=0; k < 4; k++){
+                    new_key_part[k] = temp_arr[k] ^ old_key_part[k];
+                }
+            }
+            else if(i >= words && words>6 && i%words == 4){
+                old_key_part = key_words.get(i-words);
+                old_key_part_to_modify = substitude(key_words.get(i-1));
+                for(int k=0; k < 4; k++){
+                    new_key_part[k] = old_key_part_to_modify[k] ^ old_key_part[k];
+                }
+            }
+            else{
+                old_key_part = key_words.get(i-words);
+                old_key_part_to_modify = key_words.get(i-1);
+                for(int k=0; k < 4; k++){
+                    new_key_part[k] = old_key_part_to_modify[k] ^ old_key_part[k];
+                }
+            }
+            key_words.add(new_key_part);
+        }
+
+        for(int k= round*4; k < (round+1)*4; k++){
+            for(int j=0; j<4; j++){
+                System.out.print(Integer.toHexString(key_words.get(k)[j]));
+            }
+        }
+    }
 }
